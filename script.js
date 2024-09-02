a0f855db72fbe37c046a75e701ea031d5630ffae
Rei Watanabe <164761676+Rei9005@users.noreply.github.com>
2024-09-02 13:29:00 +0900

    loading() complete()

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const quoteText = document.getElementById("quote");
 const author = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show loading
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // get quotes from api
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (!quote.author) {
     author.textContent = "Unknown";
@@ -16,15 +32,17 @@ function newQuote() {
   }
 
   if (quote.text > 120) {
-    quoteText.classList.add('long-quote')
+    quoteText.classList.add("long-quote");
   } else {
-    quoteText.classList.remove('long-quote')
+    quoteText.classList.remove("long-quote");
   }
-
+  // Set quote Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
@@ -35,9 +53,9 @@ async function getQuotes() {
   }
 }
 
-
-newQuoteBtn.addEventListener('click', newQuote);
+newQuoteBtn.addEventListener("click", newQuote);
 
 // on load
 
 getQuotes();
+// loading();
